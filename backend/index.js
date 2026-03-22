ee418470727662cd2ccb33b56eea80858b6cf2e1
sahil25123 <sahilgupta25123@gmail.com>
2026-03-22 17:54:25 +0530

    refactor: update route handling for production environment in index.js

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ const NODE_ENV = process.env.NODE_ENV;
 if (NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
