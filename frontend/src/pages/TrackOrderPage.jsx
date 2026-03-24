b45ea4d69c36055130343d5edf6c5cc5ba5bf41d
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2026-03-24 14:26:15 +0530

    refactor: enhance DeliveryBoyTracking component to include delivery and customer location data

--- frontend/src/pages/TrackOrderPage.jsx
+++ frontend/src/pages/TrackOrderPage.jsx
@@ -48,7 +48,18 @@ useEffect(()=>{
           </div>:<p className='font-semibold'>No delivery boy assigned yet.</p>}
           </>:<p className='text-green-600 font-semibold text-lg'>Delivered</p>}
           {/* Map will be shown here */}
-          {shopOrder.assignedDeliveryBoy && <DeliveryBoyTracking data={currentOrder}/>}
+          {shopOrder.assignedDeliveryBoy && 
+          <div className='h-[400px] w-full rounded-2xl overflow-hidden shadow-md'>
+          <DeliveryBoyTracking data={{
+            deliveryBoyLocation :{
+              lat: shopOrder.assignedDeliveryBoy.location.coordinates[1],
+              lon: shopOrder.assignedDeliveryBoy.location.coordinates[0],
+            },
+            customerLocation:{
+              lat:currentOrder.deliveryAddress.latitude,
+              lon:currentOrder.deliveryAddress.longitude,
+            }
+          }}/></div>}
 
         </div>
       ))}
